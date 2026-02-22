b5dcb04f4aae1bf22502d137cbddd04ec6afacf5
191599805 <191599805@qq.com>
2026-02-22 23:51:15 +0800

    刷题

---
+++ test_2.22/Project4.c
@@ -0,0 +1,133 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+	//有效的括号（检测'()''[]''{}'是否配对）
+//typedef char StackDateType; //栈遵循后进先出（先进后出）原则
+//typedef struct Stack
+//{
+//	StackDateType* arr;
+//	int top; //栈顶（自己定，可头可尾）
+//	int capacity; //数据总量
+//}ST;
+//
+////初始化栈
+//void STInis(ST* pst)
+//{
+//	assert(pst);
+//
+//	pst->arr = NULL;
+//	pst->capacity = 0;
+//	//top为数组下标
+//	//top指向栈顶元素的后一位
+//	pst->top = 0;
+//	//top指向栈顶元素
+//	//pst->top = -1;
+//}
+//
+////删除栈
+//void STDestroy(ST* pst)
+//{
+//	assert(pst);
+//
+//	pst->capacity = pst->top = 0;
+//
+//	free(pst->arr);
+//	pst->arr = NULL;
+//}
+//
+////在栈顶放入数据  (入栈)
+//void STPush(ST* pst, StackDateType x)
+//{
+//	assert(pst);
+//
+//	int newcapacity = pst->capacity != 0 ? pst->capacity * 2 : 4;
+//	if (pst->top == pst->capacity)
+//	{
+//		StackDateType* tmp = (StackDateType*)realloc(pst->arr, newcapacity * sizeof(StackDateType));
+//		if (tmp == NULL)
+//		{
+//			perror("realloc fail");
+//			exit(-1);
+//		}
+//		else
+//		{
+//			pst->arr = tmp;
+//			pst->capacity = newcapacity;
+//		}
+//	}
+//
+//	pst->arr[pst->top] = x;
+//	pst->top++;
+//}
+//
+////在栈顶删除(弹出)数据  (出栈)
+//void STPop(ST* pst)
+//{
+//	assert(pst);
+//	assert(pst->arr);
+//	assert(pst->top > 0);
+//
+//	--pst->top;
+//}
+//
+////取栈顶数据
+//StackDateType STTop(ST* pst)
+//{
+//	assert(pst);
+//	assert(pst->arr);
+//	assert(pst->top > 0);
+//
+//	return pst->arr[pst->top - 1];
+//}
+//
+////栈判空
+//bool STEmpty(ST* pst)
+//{
+//	assert(pst);
+//
+//	return pst->top == 0;
+//}
+//
+//bool isValid(char* s) {
+//	ST st;
+//	STInis(&st);
+//
+//	while (*s)
+//	{
+//		if ((*s == '(')
+//			|| (*s == '[')
+//			|| (*s == '{'))
+//		{
+//			STPush(&st, *s);
+//		}
+//		else
+//		{
+//			if (STEmpty(&st))
+//			{
+//				STDestroy(&st);
+//				return false;
+//			}
+//
+//			char top = STTop(&st);
+//			STPop(&st);
+//
+//			if ((top == '(' && *s != ')')
+//				|| (top == '[' && *s != ']')
+//				|| (top == '{' && *s != '}'))
+//			{
+//				STDestroy(&st);
+//				return false;
+//			}
+//		}
+//
+//		s++;
+//	}
+//
+//	if (!STEmpty(&st))
+//	{
+//		STDestroy(&st);
+//		return false;
+//	}
+//
+//	STDestroy(&st);
+//	return true;
+//}
